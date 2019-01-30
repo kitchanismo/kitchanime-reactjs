400acdfcfc8bf3a6335f6854afbdccd7fa097c29
kitchanismo <kitchanismo@yahoo.com>
2019-01-29 16:44:04 -0800

    test

--- src/routes.jsx
+++ src/routes.jsx
@@ -13,9 +13,9 @@ import UnAuthorized from './components/partials/unauthorized'
 const Routes = () => {
   return (
     <Switch>
-      <AuthRoute exact path="/home" component={Home} />
-      <GuestRoute exact path="/login" component={LoginForm} />
-      <GuestRoute exact path="/register" component={RegisterForm} />
+      <AuthRoute path="/home" component={Home} />
+      <GuestRoute path="/login" component={LoginForm} />
+      <GuestRoute path="/register" component={RegisterForm} />
       <AuthRoute path="/animes/:id" component={AnimeForm} />
       <Route path="/not-found" component={NotFound} />
       <Route path="/unauthorized" component={UnAuthorized} />
