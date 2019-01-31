8e248adf49e09cfd07ab64dd00681c03aec6fe46
kitchanismo <kitchanismo@yahoo.com>
2019-01-31 13:09:32 -0800

    useResource

---
+++ src/hooks/useResource.js
@@ -0,0 +1,28 @@
+import { useEffect, useReducer } from 'react'
+import { SET_ITEMS } from './types'
+
+const useResource = (request, items, args) => {
+  const reducer = (state, action) => {
+    const { payload, type } = action
+    switch (type) {
+      case SET_ITEMS:
+        return { ...state, [items]: payload }
+        return state
+    }
+  }
+  const initialState = {
+    [items]: [],
+    refresh: true
+  }
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    request({ ...args }).then(response => {
+      dispatch({ type: SET_ITEMS, payload: response[items] })
+    })
+  }, [])
+
+  return { state, dispatch }
+}
+
+export default useResource
