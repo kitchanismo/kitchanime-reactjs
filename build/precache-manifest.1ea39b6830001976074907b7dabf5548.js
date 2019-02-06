31692672c3be628211c5603f0d6c69c714761c75
kitchanismo <kitchanismo@yahoo.com>
2019-02-06 10:52:55 -0800

    include build to repo

---
+++ build/precache-manifest.1ea39b6830001976074907b7dabf5548.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "136d56d022e98e9efdc6",
+    "url": "/static/css/main.43a42804.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "c924950d661c468f15aa",
+    "url": "/static/js/1.c924950d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "136d56d022e98e9efdc6",
+    "url": "/static/js/main.136d56d0.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "c924950d661c468f15aa",
+    "url": "/static/css/1.bacd43ac.chunk.css"
+  },
+  {
+    "revision": "8fad2806321188c05bb32cc98b94db98",
+    "url": "/index.html"
+  }
+];
